e2a19cad8d12fcde03c7c03185e1fb84d47eda68
Maks <chelovek89327@gmail.com>
2026-05-04 23:00:34 +0300

    initial commit

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,34 +1,26 @@
-import unittest
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
-class TestMasks(unittest.TestCase):
-    def test_get_mask_card_number(self) -> None:
-        self.assertEqual(
-            get_mask_card_number("1234 1234 1234 1234"),
-            "1234 ** **** 1234",
-        )
-        self.assertEqual(
-            get_mask_card_number("1234"),
-            "Номер карты должен содержать 16 цифр. Попробуйте ещё раз.",
-        )
-        self.assertEqual(
-            get_mask_card_number("abcd"),
-            "Формат номера карты неверный, он должен состоять только из цифр.",
-        )
-
-    def test_get_mask_account(self) -> None:
-        self.assertEqual(get_mask_account("12345678901234567890"), "*****7890")
-        self.assertEqual(
-            get_mask_account("12345"),
-            "Номер счета должен содержать 20 цифр. Попробуйте ещё раз.",
-        )
-        self.assertEqual(
-            get_mask_account("abcde"),
-            "Формат номера счета неверный, он должен состоять только из цифр.",
-        )
+def test_get_mask_card_number() -> None:
+    assert get_mask_card_number("1234567812345678") == "1234 56** **** 5678"
+    assert get_mask_card_number("1234 5678 1234 5678") == "1234 56** **** 5678"
+    assert (
+        get_mask_card_number("1234")
+        == "Номер карты должен содержать 16 цифр. Попробуйте ещё раз."
+    )
+    assert (
+        get_mask_card_number("abcd")
+        == "Формат номера карты неверный, он должен состоять только из цифр."
+    )
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_get_mask_account() -> None:
+    assert get_mask_account("12345678901234567890") == "**7890"
+    assert (
+        get_mask_account("12345")
+        == "Номер счета должен содержать 20 цифр. Попробуйте ещё раз."
+    )
+    assert (
+        get_mask_account("abcde")
+        == "Формат номера счета неверный, он должен состоять только из цифр."
+    )
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,15 +1,22 @@
 from src.widget import get_date, mask_account_card
 
 
-def test_mask_account_card_for_card() -> None:
+def test_mask_account_card_for_visa_platinum() -> None:
     assert (
         mask_account_card("Visa Platinum 7000792289606361")
-        == "Visa Platinum 7000 ** **** 6361"
+        == "Visa Platinum 7000 79** **** 6361"
+    )
+
+
+def test_mask_account_card_for_maestro() -> None:
+    assert (
+        mask_account_card("Maestro 7000792289606361")
+        == "Maestro 7000 79** **** 6361"
     )
 
 
 def test_mask_account_card_for_account() -> None:
-    assert mask_account_card("Счет 73654108430135874305") == "Счет *****4305"
+    assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
 
 
 def test_get_date() -> None:
