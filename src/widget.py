ee3a604660fbc315b7cf7edc7e893e1ee64333c5
Maks <chelovek89327@gmail.com>
2026-05-04 23:34:02 +0300

    fix widget functions

--- src/widget.py
+++ src/widget.py
@@ -2,15 +2,21 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(data: str) -> str:
-    if data.startswith("Счет"):
-        return get_mask_account(data.split()[1])
+    """Маскирует карту или счет."""
+
+    parts = data.split()
+    name = parts[0]
+    number = parts[1]
+
+    if name == "Счет":
+        return f"{name} {get_mask_account(number)}"
     else:
-        parts = data.split()
-        return f"{parts[0]} {get_mask_card_number(parts[1])}"
+        return f"{name} {get_mask_card_number(number)}"
 
 
 def get_date(date_str: str) -> str:
-    return ".".join(reversed(date_str[:10].split("-")))
+    """Форматирует дату."""
+    return ".".join(date_str[:10].split("-")[::-1])
 
 
 
