cb782ba98804c9d0bac3896185e689ec3e17b2f8
Maks <chelovek89327@gmail.com>
2026-06-20 11:14:48 +1200

    Add logging for masks and utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,26 @@
 import json
+import logging
+from pathlib import Path
+
+log_dir = Path(__file__).resolve().parent.parent / "logs"
+log_dir.mkdir(exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+if not logger.handlers:
+    file_handler = logging.FileHandler(
+        log_dir / "utils.log",
+        mode="w",
+        encoding="utf-8",
+    )
+
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
 
 
 def load_transactions(file_path: str) -> list[dict]:
@@ -15,10 +37,17 @@ def load_transactions(file_path: str) -> list[dict]:
         with open(file_path, encoding="utf-8") as file:
             data = json.load(file)
 
-            if isinstance(data, list):
-                return data
+        if isinstance(data, list):
+            logger.info(f"Успешно загружено {len(data)} транзакций")
+            return data
 
-    except (FileNotFoundError, json.JSONDecodeError):
-        pass
+        logger.error("JSON не содержит список транзакций")
+        return []
 
-    return []
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {file_path}")
+        return []
+
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка чтения JSON: {file_path}")
+        return []
